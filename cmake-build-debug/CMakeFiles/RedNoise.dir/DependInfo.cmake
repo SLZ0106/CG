5cacfb9e0cd5e455bb3e9c8571265305b8360bba
SLZ0106 <gp19665@bristol.ac.uk>
2022-11-07 14:41:30 +0000

    Push to github from week1 to 6

---
+++ cmake-build-debug/CMakeFiles/RedNoise.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/RedNoise/libs/sdw/CanvasPoint.cpp" "CMakeFiles/RedNoise.dir/libs/sdw/CanvasPoint.cpp.obj" "gcc" "CMakeFiles/RedNoise.dir/libs/sdw/CanvasPoint.cpp.obj.d"
+  "D:/RedNoise/libs/sdw/CanvasTriangle.cpp" "CMakeFiles/RedNoise.dir/libs/sdw/CanvasTriangle.cpp.obj" "gcc" "CMakeFiles/RedNoise.dir/libs/sdw/CanvasTriangle.cpp.obj.d"
+  "D:/RedNoise/libs/sdw/Colour.cpp" "CMakeFiles/RedNoise.dir/libs/sdw/Colour.cpp.obj" "gcc" "CMakeFiles/RedNoise.dir/libs/sdw/Colour.cpp.obj.d"
+  "D:/RedNoise/libs/sdw/DrawingWindow.cpp" "CMakeFiles/RedNoise.dir/libs/sdw/DrawingWindow.cpp.obj" "gcc" "CMakeFiles/RedNoise.dir/libs/sdw/DrawingWindow.cpp.obj.d"
+  "D:/RedNoise/libs/sdw/ModelTriangle.cpp" "CMakeFiles/RedNoise.dir/libs/sdw/ModelTriangle.cpp.obj" "gcc" "CMakeFiles/RedNoise.dir/libs/sdw/ModelTriangle.cpp.obj.d"
+  "D:/RedNoise/libs/sdw/RayTriangleIntersection.cpp" "CMakeFiles/RedNoise.dir/libs/sdw/RayTriangleIntersection.cpp.obj" "gcc" "CMakeFiles/RedNoise.dir/libs/sdw/RayTriangleIntersection.cpp.obj.d"
+  "D:/RedNoise/libs/sdw/TextureMap.cpp" "CMakeFiles/RedNoise.dir/libs/sdw/TextureMap.cpp.obj" "gcc" "CMakeFiles/RedNoise.dir/libs/sdw/TextureMap.cpp.obj.d"
+  "D:/RedNoise/libs/sdw/TexturePoint.cpp" "CMakeFiles/RedNoise.dir/libs/sdw/TexturePoint.cpp.obj" "gcc" "CMakeFiles/RedNoise.dir/libs/sdw/TexturePoint.cpp.obj.d"
+  "D:/RedNoise/libs/sdw/Utils.cpp" "CMakeFiles/RedNoise.dir/libs/sdw/Utils.cpp.obj" "gcc" "CMakeFiles/RedNoise.dir/libs/sdw/Utils.cpp.obj.d"
+  "D:/RedNoise/src/RedNoise.cpp" "CMakeFiles/RedNoise.dir/src/RedNoise.cpp.obj" "gcc" "CMakeFiles/RedNoise.dir/src/RedNoise.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/RedNoise.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RedNoise.dir/libs/sdw/CanvasPoint.cpp.obj"
+  "CMakeFiles/RedNoise.dir/libs/sdw/CanvasPoint.cpp.obj.d"
+  "CMakeFiles/RedNoise.dir/libs/sdw/CanvasTriangle.cpp.obj"
+  "CMakeFiles/RedNoise.dir/libs/sdw/CanvasTriangle.cpp.obj.d"
+  "CMakeFiles/RedNoise.dir/libs/sdw/Colour.cpp.obj"
+  "CMakeFiles/RedNoise.dir/libs/sdw/Colour.cpp.obj.d"
+  "CMakeFiles/RedNoise.dir/libs/sdw/DrawingWindow.cpp.obj"
+  "CMakeFiles/RedNoise.dir/libs/sdw/DrawingWindow.cpp.obj.d"
+  "CMakeFiles/RedNoise.dir/libs/sdw/ModelTriangle.cpp.obj"
+  "CMakeFiles/RedNoise.dir/libs/sdw/ModelTriangle.cpp.obj.d"
+  "CMakeFiles/RedNoise.dir/libs/sdw/RayTriangleIntersection.cpp.obj"
+  "CMakeFiles/RedNoise.dir/libs/sdw/RayTriangleIntersection.cpp.obj.d"
+  "CMakeFiles/RedNoise.dir/libs/sdw/TextureMap.cpp.obj"
+  "CMakeFiles/RedNoise.dir/libs/sdw/TextureMap.cpp.obj.d"
+  "CMakeFiles/RedNoise.dir/libs/sdw/TexturePoint.cpp.obj"
+  "CMakeFiles/RedNoise.dir/libs/sdw/TexturePoint.cpp.obj.d"
+  "CMakeFiles/RedNoise.dir/libs/sdw/Utils.cpp.obj"
+  "CMakeFiles/RedNoise.dir/libs/sdw/Utils.cpp.obj.d"
+  "CMakeFiles/RedNoise.dir/src/RedNoise.cpp.obj"
+  "CMakeFiles/RedNoise.dir/src/RedNoise.cpp.obj.d"
+  "RedNoise.exe"
+  "RedNoise.exe.manifest"
+  "RedNoise.pdb"
+  "libRedNoise.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RedNoise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
